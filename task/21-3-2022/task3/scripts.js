eeb5e055c69c4fe31db7faa94175839f0f01b480
khaled94 <khaledbaniamer23@gmail.com>
2022-03-22 22:36:26 +0300

    main

--- task/21-3-2022/task3/scripts.js
+++ task/21-3-2022/task3/scripts.js
@@ -1,21 +1,28 @@
 console.log('Here is : While')
-/*
+
 console.log("Exercises 1")
 
 function subtract(n){
     let i = n ;
     let sub = 0
     while (n!=0) {
-        sub -= n-1
-        n--;
+        if(n>0){
+            sub -= n-1
+            n--
+        }else{
+            sub -= n+1;
+            n++;
+        }
     }
     return i+sub;
 }
+
 console.log(subtract(5))
 console.log(subtract(2))
 console.log(subtract(6))
 console.log(subtract(4))
 console.log(subtract(9))
+console.log(subtract(-4))
 
 console.log("*************")
 
@@ -24,15 +31,17 @@ console.log("Exercises 2")
 function factorial(n){
     let i = n ;
     let fact=1;
-    while(i>0){
+    while(i!=0){
         fact *=i;
-        i--;
+        
+        n>0 ? i--:i++;
         
     }
     return fact
 }
 console.log(factorial(5))
 console.log(factorial(4))
+console.log(factorial(-4))
 
 console.log("*************")
 
@@ -55,6 +64,7 @@ function repeatStr(str , n ){
     return repeat;
 }
 
+
 console.log(repeatStr("to",4))
 console.log(repeatStr("",4))
 
@@ -63,32 +73,56 @@ console.log("*************")
 console.log("Exercises 4")
 
 function sum2(a , b ){
-    return a+b
+    let sub= 0;
+    
+    while(a<=b){
+        sub+=a
+        a++;
+    }
+    return sub;
 }
 console.log(sum2(4,5))
+console.log(sum2(3, 6))
 
 console.log("*************")
 
 console.log("Exercises 5")
 
-function repeatStr2(str1 , str2){
+function repeatStr(a , b){
     let i = 0 ;
     let repeat = "";
     
-    while (i<str2) {
-        repeat += str1
+    while (i < b.length) {
+        if(i == b.length-1){
+            repeat += a
+            break
+        }
+        repeat += `${a} `
         i++
     }
     return repeat;
 }
-console.log(repeatStr2(("ro","cc")))
+console.log(repeatStr("row" , "cc"))
 
 console.log("*************")
 
 console.log("Exercises 6")
 
 function multiOf(m , n , t){
-    return m*(Math.pow(n,t))
+    let i = 0 ;
+    let x=1
+    while (i!=t) {
+        
+        if(t>0){
+            x*=n;
+            i++
+        }else{
+            x*=1/n;
+            i--
+        }
+        
+    }
+    return m*x
 }
 
 console.log(multiOf(4,10,3))
@@ -115,8 +149,9 @@ function muti2(m,n){
     return muitable;
 }
 console.log(muti2(4,2))
+console.log(muti2(3, 6))
 
-*/
+
 console.log("*************")
 
 console.log("Exercises 8")
@@ -133,7 +168,6 @@ function numberBetweenUs(m, n) {
         }
         else {
             while (i < m - n) {
-
                 count = count + `${m - i}`
                 i++
             }
@@ -143,23 +177,27 @@ function numberBetweenUs(m, n) {
     return count.split("").join(",");
 }
 console.log(numberBetweenUs(2,8));
-console.log(numberBetweenUs(3,5));
+console.log(numberBetweenUs(5,2));
 console.log(numberBetweenUs(9,5));
 
 console.log("*************")
 
 console.log("Exercises 9")
-
+let arr = []
+let str =""
 function countDown(n){
-    let str = n+" " 
-    console.log(str)
-    if(n-1>0){
+    arr.push(n)
+    if(n-1>=0){
+
         countDown(n-1)
     }
     
-    
+    str = arr.join(",")
+    return str.replace("0" , "done")
 }
-countDown(5)
+console.log(countDown(5))
+
+
 console.log("*************")
 
 console.log("Exercises 10")
