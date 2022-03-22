124a5361425669985526ca296a54af8fed82ba22
khaled94 <khaledbaniamer23@gmail.com>
2022-03-22 23:06:12 +0300

    main

--- task/21-3-2022/task2/part2.js
+++ task/21-3-2022/task2/part2.js
@@ -2,12 +2,9 @@ let num1=window.prompt('enter the first number')
 let num2=window.prompt('enter the second number')
 let num3=window.prompt('enter the three number')
 
-if(num1<0){
+
+if(num1*num2*num3<0){
     window.alert('The sign is - ')
-}else if(num2<0){
-    window.alert('The sign is - ')
-}else if(num3<0){
-    window.alert('-')
 }else{
-    window.alert("no number is - sing")
+    window.alert('The sign is + ')
 }
