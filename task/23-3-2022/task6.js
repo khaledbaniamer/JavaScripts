6d71653cff354a2cc6bcef7d68232e91523f62e9
khaled94 <khaledbaniamer23@gmail.com>
2022-03-23 21:05:33 +0300

    main

--- task/23-3-2022/task6.js
+++ task/23-3-2022/task6.js
@@ -1,17 +1,6 @@
 let songName = document.getElementById('songName');
 let submit = document.getElementById('submit')
 
-let songs = document.getElementsByClassName('songs')
-let x =""
-let count = 0;
 function addSong(){
-    
-    for(let i = 0 ; i<3; i++){
-    count = 0 
-    songs[count].innerHTML = songName.value;
-           
-    }
-    count++
-    
-
+    document.getElementById('main').innerHTML +=  songName.value +`<br>`;
 }
