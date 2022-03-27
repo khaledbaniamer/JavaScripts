43231dfb1bcdb88347bcbcdae1f5cd8a0a992a57
khaled94 <khaledbaniamer23@gmail.com>
2022-03-27 12:17:10 +0300

    main

--- oth/arr.js
+++ oth/arr.js
@@ -1,22 +1,3 @@
-// parent class
-function person (fname , lname){
-  this.firstname = fname;
-  this.lastname = lname
-}
-let x = window.prompt("please enter your gender")
-let person1 = new person("khaled","amer")
-
-
-
-
-
-person.prototype.geder = x;
-
-console.log(person1.geder)
-console.log(person1)
-
-
-
 
 
 
