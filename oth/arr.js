45bb5637279e35950a670c4761b53bfddbace616
khaled94 <khaledbaniamer23@gmail.com>
2022-03-28 05:25:45 +0300

    main

--- oth/arr.js
+++ oth/arr.js
@@ -1,4 +1,62 @@
+// let x = '{"name":"khaled" , "age":"30" , "car":"null"}'
 
+// console.log(eval(JSON.parse(x)))
+
+// let x  = new XMLHttpRequest()
+// x.open('GET','data.json')
+
+// x.onload = function(){
+//     console.log(x)
+//     let data = JSON.parse(x.responseText)
+
+//     document.write(data[0].name)
+//     console.log(data)
+// }
+// x.send()
+
+
+document.getElementById("show").addEventListener('click',function(event){
+    
+    
+    let x  = new XMLHttpRequest()
+    x.open('GET','data.json')
+   
+    
+    x.onload = function(){
+        
+        let data = JSON.parse(x.responseText);
+        console.log(data)
+        //data(data);
+        var allData =''
+        for(let i = 0 ; i<data.length;i++){
+            allData +='<tr>' +
+            '<td>'+ i +'</td>'+
+            '<td>'+data[i].name +'</td>' +
+            '<td>'+ data[i].email+'</td>' +
+            '<td>'+ data[i].phoneNumber +'</td>'+
+            '<td>'+`<img src=${data[i].url} style = 'width: 100px;height: 100px'>` +'</td>'+
+            '</tr>'
+        }
+        document.getElementById("bodytable").innerHTML += allData
+        
+        
+        document.getElementById("show").disabled = true
+    }
+    x.send()
+});
+
+// function data(y){
+//     var allData =''
+//     for(let i = 0 ; i<y.length;i++){
+//         allData+='<tr>' +
+//         '<td>'+ i +'</td>'
+//         '<td>'+y[i].name +'</td>' 
+//         '<td>'+ y[i].email+'</td>' 
+//         '<td>'+ y[i].phoneNumber +'</td>'+
+//         '</tr>'
+//     }
+//     document.getElementById("bodytable").insertAdjacentElement("beforeend" , allData)
+// }
 
 
 
