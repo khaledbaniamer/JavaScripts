1248b69e90f49f43624abdf8a7fe6e51c2a55fff
khaled94 <khaledbaniamer23@gmail.com>
2022-04-12 22:31:21 +0300

    main

--- oth/arr.js
+++ oth/arr.js
@@ -1,5 +1,27 @@
 
+// let allAdata 
+// const fs = require('fs')
+// const newData = {name:"kahled" , email:"khaled@khaled.com" , phoneNumber:"07888525852"};
+// const jsonData = JSON.stringify(newData)
 
+// let x = new XMLHttpRequest()
+
+// x.open('GET' , 'data.json')
+
+// x.onload = function(){
+//     allAdata = JSON.parse(x.responseText)
+
+    
+
+
+//     for(let i = 0 ; i< allAdata.length ; i++){
+//         document.getElementById("jsonData").innerHTML += `<h3>${allAdata[i].name} </h3> <span>${allAdata[i].email}</span> <br><span>${allAdata[i].phoneNumber}</span> <hr></h3>`
+//     }
+//     console.log(x)
+//     x.upload
+// }
+
+// x.send()
 
 
 
--- oth/scripts.js
+++ oth/scripts.js
@@ -214,7 +214,10 @@ if(len%2===0){
 // let nums1 = [1,2,3]
 // let  nums2 = [2,4,6]
 
+// const randomInt = (min , max)=>Math.floor(Math.random()*(max-min+1)+min)
 
+
+// console.log(`rbg(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`)
 
 // var romanToInt = function(s) {
 //     let sum = 0
@@ -223,18 +226,485 @@ if(len%2===0){
 //         "C": 100,"D": 500,"M": 1000
 //     }   
 //     for (let i = 0; i < s.length; i++){
-
+//         if(numbers[s[i+1]]>numbers[s[i]]&&i<s.length){
+//             sum = sum + (numbers[s[i+1]]-numbers[s[i]]);
+//             i++
+            
+//         }else{
+//             sum+=numbers[s[i]]
+            
+//         }
+        
 //     }
-
+// return sum;
 // };
 // // let s = "MCMXCIV";
 // // let s = "III";
 
 // let s = "MDCXCV"
+// s = "MCMXCIV";
+// s = "III";
+// s="D"
 // console.log(romanToInt(s))
 
+// console.log(s.at(6))
 
-const randomInt = (min , max)=>Math.floor(Math.random()*(max-min+1)+min)
+// function sortArray(array) {
+//     var temp = 0;
+//     for (var i = 0; i < array.length; i++) {
+//       for (var j = i; j < array.length; j++) {
+//         if (array[j] < array[i]) {
+//           temp = array[j];
+//           array[j] = array[i];
+//           array[i] = temp;
+//           //console.log(array[i])
+//         }
+//       }
+//     }
+//     return array;
+//   }
+
+//   console.log(sortArray([1, 2, 4, 1, 3, 4]));
+//   let list1 = [1,2,4], list2 = [1,3,4]
+//   for(let i = 0 ; i<list2.length ; i++){
+//     list1.push(list2[i])
+//     }
+//     console.log(list1)
+
+// var addTwoNumbers = function(l1, l2) {
+//     let arr =[]
+//     let p = 0
+//     if(l1[0]==0 && l2[0]==0){
+//         arr.push(0)
+//     }else{
+//     for(let i = 0 ; i<l1.length ; i++){
+//         let sum = l1[i]+l2[i]+p
+//         if(sum<10 &&sum!=NaN){
+//             arr.push(sum)
+//         }else if(sum==NaN){
+//             sum+=l1[i]+p
+//         }
+//         else {
+//             sum = sum%10 ;
+//             arr.push(sum)
+//              p = 1;          
+//         }
+//     }
+//     }
+//     return arr
+// };
+
+// let l1 = [2,4,3], l2 = [5,6,4]
+// l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// console.log(addTwoNumbers(l1,l2))
 
 
-console.log(`rbg(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`)
+// var mySqrt = function(x) {
+//     let numsTest = 0 ; 
+//     let y = x
+
+//     while(numsTest<=y){
+
+//         let z = Math.floor((y-numsTest)/2)
+//         if(x==z*z)return z;
+//         if(x>z*z) numsTest=z+1;
+//         if(x<z*z) y=z-1
+//     }
+//     return numsTest-1
+// };
+
+// console.log(mySqrt(12))
+
+
+// var merge = function(nums1, m, nums2, n) {
+//     let totalArray = []
+//     nums1.splice(m)
+//     for(let i =0 ; i<n; i++){
+//         nums1.push(nums2[i])
+//     }
+   
+//    var temp = 0;
+//    for (var i = 0; i < nums1.length; i++) {
+//      for (var j = i; j < nums1.length; j++) {
+//        if (nums1[j] < nums1[i]) {
+//          temp = nums1[j];
+//          nums1[j] = nums1[i];
+//          nums1[i] = temp;
+//        }
+//      }
+//    }
+//   return nums1
+// };
+
+// let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+
+// console.log(nums1.slice(0,m))
+// nums1.splice(m)
+// console.log(nums1)
+// console.log(merge(nums1 , m , nums2 , n))
+
+// var maxSubArray = function(nums) {
+//     let sum = 0
+//     for(let i = 0 ; i<nums.length ; i++){
+//         sum+=nums[i]
+//     }
+//     return sum
+// };
+// // console.log(maxSubArray(nums))
+// let nums = [-2,1,-3,4,-1,2,1,-5,4]
+// nums.includes
+
+// let preSum = nums[0]
+// let sum = 0 ;
+// for(let i = 0 ; i<nums.length ; i++){
+//     if(sum<0){
+//         sum=0
+//     }
+//     sum+=nums[i]
+//     preSum = Math.max(preSum , sum)
+// }
+// console.log(preSum)
+
+// var mySqrt = function(x) {
+//     let l = 0 ; 
+//     let h = x;
+//     let sqroot;
+//     while (l<=h){
+//         sqroot = parseInt((l+h)/2)
+        
+//         if(x==sqroot*sqroot) return sqroot;
+//         if(x>sqroot*sqroot) l = sqroot+1;
+//         if(x<sqroot*sqroot) h = sqroot-1;
+//     }
+
+    
+//     return l-1;
+// };
+
+// console.log(mySqrt(1))
+
+// var climbStairs = function(n) {
+//     let start = 1 ; 
+//     let end = 1 ; 
+//     for(let i = 1 ; i<n ; i++){
+//         start+=end
+//         end = start - end
+//     }
+//     return start
+// };
+
+// console.log(climbStairs(5))
+// 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 
+// var tribonacci = function(n) {
+//     let n1 = 0;
+//     let n2 = 1;
+//     let n3 = 1; 
+//     let totalNumber = 0 ;
+//     for(let i = 2 ; i < n ; i++){
+//         totalNumber = n1 + n2 + n3
+//         n1 = n2 
+//         n2=n3 
+//         n3= totalNumber;
+//     }
+//     if(n==1) totalNumber = 1
+//     return totalNumber;
+// };
+
+// console.log(tribonacci(4))
+// console.log(tribonacci(5))
+// console.log(tribonacci(25))
+
+// var thirdMax = function(nums) {
+//     let arr = []
+//     for (let i = 0 ; i<nums.length ; i++){
+//         if(!arr.includes(nums[i])){
+//             arr.push(nums[i])
+//         }
+//     }
+//     let  t = 0;
+//     for(let i = 0 ; i<arr.length ; i++){
+//         for(let j = i ; j<arr.length ; j++){
+//             if(arr[j]>arr[i]){
+//                 t = arr[j];
+//                 arr[j] = arr[i]
+//                 arr[i] = t 
+//             }
+//         }
+//     }
+//     console.log(arr)
+//     return arr[2]
+// };
+// let num1 = "9333852702227987"
+// let num2 = "85731737104263"
+
+// num1 = "11"
+// num2="12"
+
+// var addStrings = function(num1, num2) {
+
+//     console.log(num1)
+//     return `${(+num1) + (+num2) - 2}`
+// };
+
+// console.log(addStrings(num1,num2))
+
+// let nums = [1,2,3,6,3] // true
+
+// const contiansDaplicate = function (array){
+//     let x = []
+
+//     for(let i = 0 ; i<array.length ; i++){
+//         if(!x.includes(array[i])){
+//             x.push(array[i])
+//         }
+//         else{
+//             return true
+//         }
+//     }
+//     return false;
+// }
+
+// console.log(contiansDaplicate(nums))
+
+
+// var containsDuplicate = function(nums) {
+//     let arr =[];
+//     for(let i = 0 ; i<nums.length ; i++){
+//         if(!arr.includes(nums[i])){
+//             arr[i]=nums[i]
+//         }else{
+//             return true
+//         }
+//     }
+//     return false
+// };
+
+//console.log(containsDuplicate([1,2,3,4,5,6,5]))
+// console.log(containsDuplicate([15,25,35 , 25]))
+
+// var isAnagram = function(s, t) {
+//     if(s.length!=t.length){
+//         return false
+//     }
+//     let obj = {}
+//     let obj1 ={}
+//     let flag = true
+//     for(let i = 0 ; i<s.length ; i++){
+//     if(obj[s[i]] == undefined){
+//         obj[s[i]]=1
+//     }else{
+//         obj[s[i]]++
+//     }
+//     }
+//     for(let i = 0 ; i<t.length ; i++){
+//         if(obj1[t[i]] == undefined){
+//             obj1[t[i]]=1
+//         }else{
+//             obj1[t[i]]++
+//         }
+//         }
+
+   
+//     for(let i = 0 ; i<s.length ; i++){
+//     if(obj[s[i]]!=obj1[s[i]]){
+//         return false
+//     }
+//     }
+    
+//     return true
+// };
+// let t = "anagram", s = "nagaram"
+// console.log(isAnagram(s,t))
+
+// let obj = {a: 3, n: 1, g: 1, r: 1, m: 1}
+// let obj1 ={n: 1, a: 3, g: 1, r: 1, m: 1}
+// console.log(Object.keys(obj).length)
+// let flag = true
+// for(let i = 0 ; i<s.length ; i++){
+//     if(obj[s[i]]!==obj1[s[i]]){
+//         flag = false
+//         break
+//     }
+
+// }
+// console.log(flag)
+// let g = "b"
+// console.log(g.charCodeAt())
+// console.log(s.split('').sort().join(''))
+// console.log(t.split('').sort().join(''))
+// s.split('').sort().join('') == t.split('').sort().join('')
+
+// var numIdenticalPairs = function(nums) {
+//     let count =0 ;
+//     let arr =[]
+//     for(let i = 0 ; i<nums.length ; i++){
+//         if(!arr.includes(nums[i])){
+//             arr[i]=nums[i]
+//         }else{
+//             count++
+//         }
+//     }
+//     return count
+// };
+
+// let nums = [1,2,3,1,1,3]
+// // nums = [1,1,1,1]
+// // nums = [1,2,3]
+// console.log(numIdenticalPairs(nums))
+
+// var numJewelsInStones = function(jewels, stones) {
+//     let count = 0
+//     let arr=[]
+//     let arr1 = []
+//     for(let i = 0 ; i<jewels.length;i++){
+//         arr[i]=jewels[i]
+//     }
+//     for(let i = 0 ; i<stones.length ; i++){
+//         if(arr.includes(stones[i])){
+//             count++
+//         }
+//     }
+//     return count
+// };
+
+// let jewels = "aA", stones = "aAAbbbb"
+// // jewels = "ebd"
+// // stones = "bbb"
+// console.log(numJewelsInStones(jewels,stones))
+// let c=0;
+// for(let i = 0 ; i<stones.length;i++){
+    
+//     if(jewels.includes(stones[i])){
+//         c++
+//     }
+// }
+
+// console.log(c)
+
+//1365. How Many Numbers Are Smaller Than the Current Number  -------- hashing OR hash table --- leetcode
+
+// var smallerNumbersThanCurrent = function(nums) {
+//     let arr =[]
+//     let count = 0 ; 
+//     let itration = 0
+
+//     for(let i=0 ; i<nums.length ; i++){
+//         for(let j = 0 ; j<nums.length ; j++){
+//             if(nums[j]<nums[i]){
+//                 count++
+//             }
+//             if(count == nums.length-1){
+//                 nums.splice(i,i+1)
+//             }
+//         }
+//         arr[i]=count
+//         count = 0
+//     }
+//     return arr
+// };
+
+// let nums = [8,1,2,2,3]
+// // nums = [6,5,4,8]
+// console.log(smallerNumbersThanCurrent(nums))
+
+// var smallerNumbersThanCurrent = function(nums) {
+//     nums.map(a => {
+//         a<a
+        
+//     })
+// };
+
+// let nums = [8,1,2,2,3]
+// // nums = [6,5,4,8]
+// console.log(smallerNumbersThanCurrent(nums))
+
+//2006. Count Number of Pairs With Absolute Difference K  -------- hashing OR hash table --- leetcode
+
+
+// var countKDifference = function(nums, k) {
+//     let count = 0 ;
+//     let arr=[]
+
+//     for(let i = 0 ; i<nums.length ; i++){
+//         for(let j =i ; j<nums.length ; j++){
+//             if(Math.abs(nums[i]-nums[j]) == k){
+//                 count++
+//             }
+//         }
+//     }
+//    return count
+// };
+
+// let nums = [1,2,2,1], k = 1
+
+// console.log(countKDifference(nums,k))
+
+//1684. Count the Number of Consistent Strings  -------- hashing OR hash table --- leetcode
+
+// var countConsistentStrings = function(allowed, words) {
+//     let count = 0 ;
+//     let flag = true;
+    
+//     for(let i = 0 ; i<words.length ; i++){
+//         let sp = words[i]
+//         for(let j = 0 ; j<sp.length ; j++){
+//             if(!allowed.includes(sp[j])){
+//               flag=false  
+//             }
+//         }
+//         if(flag){
+//             count++
+//         }
+//         flag = true
+//     }
+//     return count;
+// };
+
+
+
+//soluation 2 
+// var countConsistentStrings = function(allowed, words) {
+//     let objHash = {}
+    
+//     let count =0;
+//     for(let i = 0 ; i<allowed.length ; i++){
+        
+//         objHash[allowed[i]]=1
+//     }
+//     for(let i = 0 ; i<words.length ; i++){
+//         let arr=[]
+//         let sp = words[i].split("")
+//         for(let j = 0 ; j<sp.length ; j++){
+//             if(objHash[sp[j]]==undefined){
+//               arr[j]=false  
+//             }else{
+//                 arr[j]=true
+//             }
+//         }
+//         if(!arr.includes(false)){
+//             count++
+//         }
+//     }
+//     return count;
+// };
+
+// let allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+// // countConsistentStrings(allowed,words)
+// allowed = "abc"
+// words = ["a","b","c","ab","ac","bc","abc"]
+
+// allowed = "cad"
+// words = ["cc","acd","b","ba","bac","bad","ac","d"]
+
+// allowed = "pwqcdyrjhoizekt"
+// words = ["ipeihtzjwq","rwrcci","n","ktrjyhh","zipdehh","cqe","ptwwe","c","cckq","qwdqiddh","kfriwenu","y","evbvg"] //13
+// console.log(countConsistentStrings(allowed,words))
+
+//2103. Rings and Rods -------- hashing OR hash table --- leetcode
+var countPoints = function(rings) {
+    
+};
+
+let rings = "B0B6G0R6R0R6G9"
+countPoints(rings)
+// console.log(countPoints(rings))
