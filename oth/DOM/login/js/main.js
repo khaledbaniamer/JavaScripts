8416685e543df01e6b571411dcdceb09d0746893
khaled94 <khaledbaniamer23@gmail.com>
2022-04-20 04:39:02 +0300

    main

--- oth/DOM/login/js/main.js
+++ oth/DOM/login/js/main.js
@@ -7,25 +7,95 @@ const all = document.getElementById("all")
 
 const usernamePattern = /^[a-z ]+$/
 const passwordPattern = /[A-Z]{1,2}[a-zA-z0-9$!]{7,8}/
+let  studentData;
 
-
-
-btnLogin.addEventListener("click",function(e){
-    e.preventDefault()
-    
-    if(usernamePattern.test(username.value)){
+class Student{
+    constructor(id , name , math , phy , chim){
+        this.id = id;
+        this.name = name;
+        this.math = math;
+        this.pyh = phy;
+        this.chim = chim;
+    }
+}
+class showData {
+    static displayData(student){
+        let welcome = document.createElement("h2")
         
-        AllPage.connectJson()
-    }else{
-        UI.showError("- user name should have letter only" +"<br>"+  "- password should beging with capital letter and must be 8 character" , "danger")
+        welcome.innerHTML = `<br> Welcome ${student.name} <br> <br>`
+
+        let table = document.createElement("table")
+        table.className = "table"
+        table.innerHTML = `
+        <tr>
+            <th>Student ID</th>
+            <th>Math</th>
+            <th>Physics</th>
+            <th>Chemical</th>
+        </tr>
+
+        <tr>
+            <td>${student.id}</td>
+            <td>${student.math}</td>
+            <td>${student.physics}</td>
+            <td>${student.chemical}</td>
+            
+        </tr>
+        `
+        all.innerHTML = ""
+        all.appendChild(welcome)
+        all.appendChild(table)
+    }
+    static calculatAvg (math , phy , ch){
+        let avg = (parseInt(math)+parseInt(phy)+parseInt(ch))/3
+        console.log(math , phy , ch)
+        let label = document.createElement("label")
+        label.className = "form-label"
+
+        label.innerHTML = `<br> Your Avarage is : ${avg}`
+        all.appendChild(label)
     }
 
-    if(passwordPattern.test(password.value)){
-
-    }else{
-        UI.showError("- user name should have letter only" +"<br>"+  "- password should beging with capital letter and must be 8 character" , "danger")
+    static clear(){
+        username.value = ""
+        password.value = ""
     }
+    static error(message , className){
+        labelPassword.className = `mt-2 alert alert-${className}`
+        labelPassword.innerHTML = message
+    }
+}
+
+
+
+btnLogin.addEventListener("click" , function(e){
+    e.preventDefault()
+    const x = new XMLHttpRequest()
+    x.open("GET" , "mark.json")
+
+    x.onload = function(){
+    studentData = JSON.parse(x.responseText)
+    console.log(studentData)
+    
+    for(let i of studentData){
+        if(username.value == i.name && password.value == i.pin){
+            console.log("username exist")
+            showData.clear()
+            showData.displayData(i)
+            showData.calculatAvg(i.math , i.physics , i.chemical)
+        }else{
+            showData.error("user name or password wrong !" , "danger")
+        }
+    }
+
+
+    }
+x.send()
 })
+
+
+
+
 
 
 
